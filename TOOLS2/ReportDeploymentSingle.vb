71240e10ee5af253e476a0749b4b422967fa6f55
skumar <skumar@lakeco.com>
2025-07-16 10:17:46 -0500

    Initial checkin

---
+++ TOOLS2/ReportDeploymentSingle.vb
@@ -0,0 +1,116 @@
+Dim definition As [Byte]() = Nothing
+Dim warnings As Warning() = Nothing
+Dim parentPath As String = "/" + parentFolder
+
+
+Public Sub Main()
+
+    rs.Credentials = System.Net.CredentialCache.DefaultCredentials
+    Dim name As String
+
+    ''Create the SyteLineReport parent folder
+    'Try
+    '    rs.CreateFolder(parentFolder, "/", Nothing)
+    '    Console.WriteLine("Parent folder created: {0}", parentFolder)
+    'Catch e As Exception
+    '    Console.WriteLine(e.Message)
+    'End Try
+
+    ''Publish report logo
+    'PublishImage("SLHeaderLogo.bmp")
+
+    'Publish all SyteLine Reports
+    'Dim storefile As Directory
+    'Dim files As String()
+    'Dim File As String
+
+    'files = storefile.GetFiles(filePath, "*.rdl")
+
+    'For Each File In files
+    '    Dim s() As String = File.Split("\")
+    '    Console.WriteLine("File: " + s(s.Length - 1))
+    '    PublishReport(s(s.Length - 1))
+    'Next
+    PublishReport()
+End Sub
+
+Public Sub PublishReport()
+
+    Try
+        'read in report xml
+        Dim stream As FileStream = File.OpenRead(filePath + "\" + reportName)
+        definition = New [Byte](stream.Length - 1) {}
+        stream.Read(definition, 0, CInt(stream.Length))
+        stream.Close()
+    Catch e As IOException
+        Console.WriteLine(e.Message)
+    End Try
+
+    Try
+        'create report
+        Dim reportNameStr() As String = reportName.Split(New [Char]() {"."c}, System.StringSplitOptions.None)
+	Try
+	    rs.DeleteItem(parentPath + "/" + reportNameStr(0))
+	Catch e1 As Exception
+            'Console.WriteLine(e1.Message)
+	End Try
+        warnings = rs.CreateReport(reportNameStr(0), parentPath, True, definition, Nothing)
+
+        'display warnings
+        If Not (warnings Is Nothing) Then
+            Dim warning As Warning
+            For Each warning In warnings
+                Console.WriteLine(warning.Message)
+            Next warning
+        Else
+            Console.WriteLine("Report: {0} published successfully with no warnings", reportName)
+        End If
+    Catch e As Exception
+
+        If e.Message.Contains("hexadecimal value 0x00") Then
+            PublishReport_Upgraded(reportName)
+        Else
+            Console.WriteLine(e.Message)
+        End If
+
+    End Try
+
+End Sub
+
+Public Sub PublishReport_Upgraded(ByVal reportName As String)
+
+    Try
+        'read in report xml
+        Dim stream As FileStream = File.OpenRead(filePath + "\" + reportName)
+        definition = New [Byte](stream.Length) {}
+        stream.Read(definition, 0, CInt(stream.Length))
+        stream.Close()
+    Catch e As IOException
+        Console.WriteLine(e.Message)
+    End Try
+
+    Try
+        'create report
+        Dim reportNameStr() As String = reportName.Split(New [Char]() {"."c}, System.StringSplitOptions.None)
+	Try
+	    rs.DeleteItem(parentPath + "/" + reportNameStr(0))
+	Catch e1 As Exception
+            'Console.WriteLine(e1.Message)
+	End Try
+        warnings = rs.CreateReport(reportNameStr(0), parentPath, True, definition, Nothing)
+
+        'display warnings
+        If Not (warnings Is Nothing) Then
+            Dim warning As Warning
+            For Each warning In warnings
+                Console.WriteLine(warning.Message)
+            Next warning
+        Else
+            Console.WriteLine("Report: {0} published successfully with no warnings", reportName)
+        End If
+    Catch e As Exception
+        Console.WriteLine(e.Message)
+    End Try
+
+End Sub
+
